7235de3337787da3a367328f90fa69a7e6477a2f
Suhas-Borkar <suhasborkar80@gmail.com>
2026-03-13 22:52:13 +0530

    add * in allowed host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zfk%vvs%#op5tc70w(=srnf^ts-l+$2ro=il(9#8$bdpvrf3&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
